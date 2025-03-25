7d4e134d7a74bfd4e29b082a63c2b78e133b9b78
Samueljain26 <samueljain77@gmail.com>
2025-03-25 15:42:12 +0530

    [Samuel] ADD:finding peak element and its index.

---
+++ PeakElement.java
@@ -0,0 +1,27 @@
+public class PeakElement {
+    public static int findPeak(int[] arr) {
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 20, 4, 1, 0};
+        int peakIndex = findPeak(arr);
+        System.out.println("Peak Element Index: " + peakIndex);
+        System.out.println("Peak Element: " + arr[peakIndex]);
+    }
+}
+/*Peak Element Index: 2
+Peak Element: 20
+ */
